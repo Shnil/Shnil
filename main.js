60722afc21408c6b374845c5e1387d076b514a28
Noat <109909566+Shnil@users.noreply.github.com>
2022-08-10 23:20:01 +0700

    Update main.js

--- main.js
+++ main.js
@@ -15,37 +15,44 @@ function login(){
 }
 
 function signUp() {
-    var signupnhap = document.getElementById('signupnhap');
-    var signuppass = document.getElementById('signuppass');
+    var Username = document.getElementById('signupnhap').value;
+    var Password = document.getElementById('signuppass').value;
     var agree = document.getElementById('t&c');
-    if(signupnhap.value.length == 0 && signuppass.value.length == 0){
+    if(Username.length == 0 && Password.length == 0){
         alert('Please fill in Username and Password');
-    } else if(signupnhap.value.length == 0){
+    } else if(Username.length == 0){
         alert('Please fill in Username');
-    }
-    else if(signuppass.value.length == 0){
+    } else if(Password.length == 0){
         alert('Please fill in Password');
-    }
-    else if(!agree.checked){
+    } else if(!agree.checked){
         alert('Please agree the terms & conditions')
-    } else{
-        localStorage.setItem('signupnhap', signupnhap.value);
-        localStorage.setItem('signuppass', signuppass.value);
-        alert('Your account has been created');
-        document.getElementById("signup").reset();
+    } else {
+        let data = JSON.parse(localStorage.getItem('data')) || [];
+        let exist = data.length && JSON.parse(localStorage.getItem('data')).some(data => data.Username.toLowerCase() == Username.toLowerCase());
+        if(!exist){
+            data.push({ Username, Password });
+            localStorage.setItem('data', JSON.stringify(data));
+            document.getElementById('signupnhap').focus();
+            alert("Account created");
+        } else{
+            alert("Account already existed");
+        }
     }
+    console.log(JSON.parse(localStorage.getItem('data')));
 }
 
-function logIn(){
-    var storedName = localStorage.getItem('signupnhap');
-    var storedPw = localStorage.getItem('signuppass');
-
-    var userName = document.getElementById('loginnhap');
-    var userPw = document.getElementById('loginpass');
-    
-    if(userName.value == storedName && userPw.value == storedPw){
-        alert('Logged in. Welcome ' + userName.value);
-    }else{
-        alert('Wrong Password or Username');
+function logIn() {
+    var username = document.getElementById('loginnhap').value,
+        password = document.getElementById('loginpass').value;
+        
+    let data = JSON.parse(localStorage.getItem('data'));
+    let exist = data.length && JSON.parse(localStorage.getItem('data')).some(data => data.Username.toLowerCase() == username && data.Password.toLowerCase() == password);
+    if(!exist){
+        alert("Incorrect login credentials");
+        console.log(JSON.parse(localStorage.getItem('data')));
+    }
+    else{
+        alert("Logged in. Welcome " + `${username}`);
+        console.log(JSON.parse(localStorage.getItem('data')));
     }
 }
